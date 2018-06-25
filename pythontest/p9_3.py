b6ca4a8a0d337560f36d4525f73781c196e08ab9
admin <admin@admin.com>
2018-06-25 14:56:19 +0900

    konnnitiha

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -1,13 +1,25 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-steps = [6543, 7000, 8900, 10789, 3467, 11045, 5095]
-labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-num_bars = len(steps)
-positions = range(1, num_bars+1)
-plt.barh(positions, steps, align='center') 
 
-plt.yticks(positions, labels)
-plt.xlabel('Steps')
-plt.ylabel('Day')
-plt.title('Number of steps walked')
-plt.grid() plt.show()
+def plot_angle(ax, x, y, angle, style):
+    phi = np.radians(angle)
+    xx = [x + .5, x, x + .5*np.cos(phi)]
+    yy = [y, y, y + .5*np.sin(phi)]
+    ax.plot(xx, yy, lw=8, color='blue', solid_joinstyle=style)
+    ax.plot(xx[1:], yy[1:], lw=1, color='black')
+    ax.plot(xx[1::-1], yy[1::-1], lw=1, color='black')
+    ax.plot(xx[1:2], yy[1:2], 'o', color='red', markersize=3)
+    ax.text(x, y + .2, '%.0f degrees' % angle)
+
+fig, ax = plt.subplots()
+ax.set_title('Join style')
+
+for x, style in enumerate((('miter', 'round', 'bevel'))):
+    ax.text(x, 5, style)
+    for i in range(5):
+        plot_angle(ax, x, i, pow(2.0, 3 + i), style)
+
+ax.set_xlim(-.5, 2.75)
+ax.set_ylim(-.5, 5.5)
+plt.show()
