428be9e72d0f86ac54bfb0bf707c8bd34f39709c
admin <admin@admin.com>
2018-07-09 16:57:13 +0900

    second time for database

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+
 class Post(models.Model):
 	author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
@@ -9,7 +10,7 @@ class Post(models.Model):
 	published_date = models.DateTimeField(blank=True,null=True)
 
 	def publish(self):
-		self.publiished_date = timezone.now()
+		self.published_date = timezone.now()
 		self.save()
 
 	def __str__(self):
